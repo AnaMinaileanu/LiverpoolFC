c69afd84328201d6d04740ad9774b05970b58b89
AnaMinaileanu <minaileanuana@yahoo.com>
2022-05-19 14:59:18 +0300

    Add upcoming and past fixtures for each competition

---
+++ epl.js
@@ -0,0 +1,12 @@
+function showDiv() {
+    document.getElementById('video').style.display = "flex";
+    document.getElementById('video').style.alignContent ="center";
+    document.getElementById('video').style.justifyContent = "center";
+    
+ }
+
+ function showDiv2() {
+    document.getElementById('video2').style.display = "flex";
+    document.getElementById('video2').style.alignContent ="center";
+    document.getElementById('video2').style.justifyContent = "center";
+ }
